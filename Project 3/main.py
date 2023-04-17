ca8b516f7eb12035575f573e360df1f4292006be
Kiarash Mokhtari <mokhtarikiarash@gmail.com>
2023-04-17 06:05:50 +0330

    Move properties dictionary to properties.py

--- Project 3/main.py
+++ Project 3/main.py
@@ -1,8 +1,9 @@
 import random
 import copy
 import time
+from properties import *
 
-MAX_ROUNDS = 30
+MAX_ROUNDS = 1000
 
 '''
 Initializing game board with builtin dictionary data structure
@@ -15,368 +16,12 @@ Initializing game board with builtin dictionary data structure
     6. No sell option(yet).
     7. if player land on 'Go Jail' goes to 'jail' and must give 100$ and start the game from there
 '''
-# properties
-properties = {0: {"name": "Go",
-                  "price": 0,
-                  "rent": 0, 
-                  "color": "none", 
-                  "type": "corner", 
-                  "owner": "none", 
-                  "houses": 0, 
-                  "hotels": 0},
-              
-              1: {"name": "Mediterranean Avenue", 
-                  "price": 60, 
-                  "hPrice": 50, 
-                  "rent": 2, 
-                  "color": "brown", 
-                  "type": "property", 
-                  "owner": "none", 
-                  "houses": 0, 
-                  "hotels": 0},
-              
-              2: {"name": "Baltic Avenue", 
-                  "price": 60, 
-                  "hPrice": 50, 
-                  "rent": 4, 
-                  "color": "brown", 
-                  "type": "property", 
-                  "owner": "none", 
-                  "houses": 0, 
-                  "hotels": 0},
-              
-              3: {"name": "Income Tax", 
-                  "price": 0, 
-                  "rent": 0, 
-                  "color": "none", 
-                  "type": "tax",
-                  "tax_price": 200,
-                  "owner": "none", 
-                  "houses": 0, 
-                  "hotels": 0},
-              
-              4: {"name": "Reading Railroad", 
-                  "price": 200, 
-                  "rent": 25, 
-                  "color": "none", 
-                  "type": "railroad", 
-                  "owner": "none", 
-                  "houses": 0, 
-                  "hotels": 0},
-              
-              5: {"name": "Oriental Avenue", 
-                  "price": 100, 
-                  "rent": 6,
-                  "hPrice": 50, 
-                  "color": "light blue", 
-                  "type": "property", 
-                  "owner": "none", 
-                  "houses": 0, 
-                  "hotels": 0},
-              
-              6: {"name": "Vermont Avenue", 
-                  "price": 100, 
-                  "hPrice": 50, 
-                  "rent": 6, 
-                  "color": "light blue", 
-                  "type": "property", 
-                  "owner": "none", 
-                  "houses": 0, 
-                  "hotels": 0},
-              
-              7: {"name": "Connecticut Avenue", 
-                  "price": 120, 
-                  "hPrice": 50, 
-                  "rent": 8, 
-                  "color": "light blue", 
-                  "type": "property", 
-                  "owner": "none", 
-                  "houses": 0, 
-                  "hotels": 0},
-              
-              8: {"name": "Just Visiting/Jail", 
-                   "price": 0, 
-                   "rent": 0, 
-                   "color": "none", 
-                   "type": "corner", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              9: {"name": "St. Charles Place", 
-                   "price": 140, 
-                   "hPrice": 100, 
-                   "rent": 10, 
-                   "color": "pink", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              10: {"name": "Electric Company",
-                   "price": 150, 
-                   "rent": 20, 
-                   "color": "none", 
-                   "type": "utility", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              11: {"name": "States Avenue", 
-                   "price": 140, 
-                   "hPrice": 100, 
-                   "rent": 10, 
-                   "color": "pink", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              12: {"name": "Virginia Avenue", 
-                   "price": 160, 
-                   "hPrice": 100, 
-                   "rent": 12, 
-                   "color": "pink", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              13: {"name": "Pennsylvania Railroad", 
-                   "price": 200, 
-                   "rent": 25, 
-                   "color": "none", 
-                   "type": "railroad", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              14: {"name": "St. James Place", 
-                   "price": 180, 
-                   "hPrice": 100, 
-                   "rent": 14, 
-                   "color": "orange", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              15: {"name": "Tennessee Avenue", 
-                   "price": 180, 
-                   "hPrice": 100, 
-                   "rent": 14, 
-                   "color": "orange", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              16: {"name": "New York Avenue", 
-                   "price": 200, 
-                   "hPrice": 100, 
-                   "rent": 16, 
-                   "color": "orange", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              17: {"name": "Free Parking", 
-                   "price": 0, 
-                   "rent": 0, 
-                   "color": "none", 
-                   "type": "corner", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              18: {"name": "Kentucky Avenue", 
-                   "price": 220, 
-                   "hPrice": 150, 
-                   "rent": 18, 
-                   "color": "red", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              19: {"name": "Indiana Avenue", 
-                   "price": 220, 
-                   "hPrice": 150, 
-                   "rent": 18, 
-                   "color": "red", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              20: {"name": "Illinois Avenue", 
-                   "price": 240, 
-                   "hPrice": 150, 
-                   "rent": 20, 
-                   "color": "red", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              21: {"name": "B. & O. Railroad", 
-                   "price": 200, 
-                   "rent": 25, 
-                   "color": "none", 
-                   "type": "railroad", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              22: {"name": "Atlantic Avenue", 
-                   "price": 260, 
-                   "hPrice": 150, 
-                   "rent": 22, 
-                   "color": "yellow", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              23: {"name": "Ventnor Avenue", 
-                   "price": 260, 
-                   "hPrice": 150, 
-                   "rent": 22, 
-                   "color": "yellow", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              24: {"name": "Water Works", 
-                   "price": 150, 
-                   "rent": 20, 
-                   "color": "none", 
-                   "type": "utility", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              25: {"name": "Marvin Gardens", 
-                   "price": 280, 
-                   "hPrice": 150, 
-                   "rent": 24, 
-                   "color": "yellow", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              26: {"name": "Go To Jail", 
-                   "price": 0, 
-                   "rent": 0, 
-                   "color": "none", 
-                   "type": "corner", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              27: {"name": "Pacific Avenue", 
-                   "price": 300, 
-                   "hPrice": 200, 
-                   "rent": 26, 
-                   "color": "green", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              28: {"name": "North Carolina Avenue", 
-                   "price": 300, 
-                   "hPrice": 200, 
-                   "rent": 26, 
-                   "color": "green", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              29: {"name": "Pennsylvania Avenue", 
-                   "price": 320, 
-                   "hPrice": 200, 
-                   "rent": 28, 
-                   "color": "green", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              30: {"name": "Short Line", 
-                   "price": 200, 
-                   "rent": 25, 
-                   "color": "none", 
-                   "type": "railroad", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              31: {"name": "Park Place", 
-                   "price": 350, 
-                   "hPrice": 200, 
-                   "rent": 35, 
-                   "color": "dark blue", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              32: {"name": "Luxury Tax", 
-                   "price": 0, 
-                   "rent": 0, 
-                   "color": "none", 
-                   "type": "tax", 
-                   "tax_price": 100,
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0},
-              
-              33: {"name": "Boardwalk",
-                   "price": 400, 
-                   "hPrice": 200, 
-                   "rent": 50, 
-                   "color": "dark blue", 
-                   "type": "property", 
-                   "owner": "none", 
-                   "houses": 0, 
-                   "hotels": 0}
-             }
-
-all_actions = {
-    0: "NOTHING",
-    1: "BUY_PROP",
-    2: "PAY_RENT",
-    3: "PAY_TAX",
-    4: "BUILD_HOUSE",
-    5: "BUILD_HOTEL",
-    6: "JAIL_FREE"        
-}
-
-probability = {
-    2: 1 / 36,
-    3: 2 / 36,
-    4: 3 / 36,
-    5: 4 / 36,
-    6: 5 / 36,
-    7: 6 / 36,
-    8: 5 / 36,
-    9: 4 / 36,
-    10: 3 / 36,
-    11: 2 / 36,
-    12: 1 / 36,
-}
-
 class stats:
     def __init__(self, id, location, balance, jail, ownedP, ownedRR, ownedUT):
         self.id = id # store the id of each player (int: 1 , 2)
         self.location = location  # store the location of the player (string or index of dictionary)
         self.balance = balance  # store the amount of money the player has (int data)
-        self.jail = jail  # store whether the player is in jail or not (bool data)
+        self.jail = jail  # store whether the player is in jail or not (bool data) (for future updates!!)
         self.ownedP = ownedP  # store the properties that the player has bought (list of property names or index)
         self.ownedRR = ownedRR  # store the railroads that the player has bought (list of railroad names or index)
         self.ownedUT = ownedUT  # store the utilities that the player has bought (list of utility names or index)
@@ -386,9 +31,9 @@ players = {}
 for count in range(1, 3):
      players.update({count: stats(count, 0, 1500, False, [],[], [])})
 
-number_of_players = 2 #player
-current_player = 1 #current
-player_left = number_of_players #pleft
+number_of_players = 2
+current_player = 1
+player_left = number_of_players
 round_num = 0
 
 # defining some important functions:
@@ -490,7 +135,7 @@ def get_valid_actions(player):
           # Check if the property is unowned
           if properties[player.location]["owner"] == "none":
                # Add the "BUY_PROP" action
-               actions.append(all_actions[0]) # need to fix this(but latter)
+               actions.append(all_actions[0])
                actions.append(all_actions[1])
 
           elif properties[player.location]["owner"] != player.id:
