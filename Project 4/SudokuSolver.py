4995de6952d5bbae9bf49ab1b36dbd095174ce34
Kiarash Mokhtari <mokhtarikiarash@gmail.com>
2023-05-06 10:05:08 +0330

    added backtracking function

--- Project 4/SudokuSolver.py
+++ Project 4/SudokuSolver.py
@@ -41,6 +41,8 @@ class SudokuCSP:
     # This mehtod uses Minimum Remaining Values(MRV) heuristic in its domain.
     def selectUnassignedVariable(self, assignment):
         unassigned_variables = [var for var in self.variables if var not in assignment]
+        if unassigned_variables == []:
+            return None
         min_var = unassigned_variables[0]
         for var in unassigned_variables:
             if len(self.domain[var]) < len(self.domain[min_var]):
@@ -48,15 +50,72 @@ class SudokuCSP:
         return min_var
     
     def orderDomainValues(self, var):
+        if var == None:
+            return None
         return sorted(self.domain[var])                 
 
-    
+def isConsistent(var, value, assignment, sudoku):
+    # Check row consistency
+    for j in range(len(sudoku.board[0])):
+        if (var[0], j) in assignment and assignment[(var[0], j)] == value:
+            return False
 
+    # Check column consistency
+    for i in range(len(sudoku.board)):
+        if (i, var[1]) in assignment and assignment[(i, var[1])] == value:
+            return False
 
+    # Check subgrid consistency
+    subgrid_i = (var[0] // sudoku.subgrid_size) * sudoku.subgrid_size
+    subgrid_j = (var[1] // sudoku.subgrid_size) * sudoku.subgrid_size
+    for i in range(subgrid_i, subgrid_i + sudoku.subgrid_size):
+        for j in range(subgrid_j, subgrid_j + sudoku.subgrid_size):
+            if (i, j) in assignment and assignment[(i, j)] == value:
+                return False
 
+    return True
 
+def recursiveBacktracking(sudoku: SudokuCSP, assignment: dict, unassigned_variables: list):
+    # Check if the assignment is complete
+    if len(assignment) == len(sudoku.variables):
+        return [[assignment[(i, j)] for j in range(len(sudoku.board[0]))] for i in range(len(sudoku.board))]
 
+    # Select an unassigned variable
+    var = sudoku.selectUnassignedVariable(assignment)
 
+    # Order the domain of the variable
+    ordered_domain = sudoku.orderDomainValues(var)
+
+    # Try each value in the ordered domain
+    for value in ordered_domain:
+        # Check if the value is consistent with the current assignment
+        if isConsistent(var, value, assignment, sudoku):
+            # Add the value to the assignment
+            assignment[var] = value
+            unassigned_variables.remove(var)
+            print(assignment)
+
+            # Recursively call the function with the updated assignment and list of unassigned variables
+            result = recursiveBacktracking(sudoku, assignment, unassigned_variables)
+
+            # If the recursion returns a solution, return the solution
+            if result != None:
+                return result
+
+            # If the recursion does not return a solution, remove the value from the assignment
+            del assignment[var]
+            unassigned_variables.append(var)
+
+    # If no value in the domain results in a solution, return failure
+    return None
+
+def solve(board):
+    sudoku = SudokuCSP(board)
+    sudoku.setVariables()
+    sudoku.setDomains()
+    assignment = {}
+    unassigned_variables = sudoku.variables
+    return recursiveBacktracking(sudoku, assignment, unassigned_variables)
 
 def printSudoku(board):
     for i in range(len(board)):
@@ -95,14 +154,6 @@ def setBoard():
     return board
 
 board = setBoard()
-print(board)
-csp = SudokuCSP(board)
-csp.setVariables()
-# print(csp.variables)
-print(csp.subgrid_size)
-csp.setDomains()
-print(csp.domain)
-print(csp.selectUnassignedVariable({}))
-print(csp.orderDomainValues((3,4)))
-#solve(board)
 printSudoku(board)
+assignment = solve(board)
+print(assignment)
